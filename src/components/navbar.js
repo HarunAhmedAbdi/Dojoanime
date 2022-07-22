0316fc7ca1dd0eb588727c25f7352e492cbfc1f1
Harun <harunahmedabdi@gmail.com>
2022-07-22 10:59:53 +0100

    fixed navbar links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,21 +17,10 @@ function NavScroll() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Top Airing</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/seasonal">Seasonal</Nav.Link>
+            <Nav.Link href="/top-airing">Top Airing</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
